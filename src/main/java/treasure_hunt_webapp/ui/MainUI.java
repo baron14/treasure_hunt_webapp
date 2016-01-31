7d1c4956e5efdab010dd9363a9afd5dbf7eed12c
Jordan <baron14@hotmail.co.uk>
2016-01-31 22:48:19 +0000

    finished basic GUI

--- src/main/java/treasure_hunt_webapp/ui/MainUI.java
+++ src/main/java/treasure_hunt_webapp/ui/MainUI.java
@@ -1,30 +1,46 @@
 package treasure_hunt_webapp.ui;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 import treasure_hunt_webapp.custom.component.ItemPanel;
 import treasure_hunt_webapp.dao.RouteDao;
+import treasure_hunt_webapp.models.route.HeartRate;
+import treasure_hunt_webapp.models.route.Point;
+import treasure_hunt_webapp.models.route.Question;
 import treasure_hunt_webapp.models.route.Route;
 import treasure_hunt_webapp.models.route.Step;
 
 import com.vaadin.annotations.Theme;
+import com.vaadin.annotations.VaadinServletConfiguration;
+import com.vaadin.data.Property.ValueChangeEvent;
+import com.vaadin.data.Property.ValueChangeListener;
 import com.vaadin.event.FieldEvents.TextChangeEvent;
 import com.vaadin.event.FieldEvents.TextChangeListener;
+import com.vaadin.server.FontAwesome;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.spring.annotation.SpringUI;
+import com.vaadin.tapio.googlemaps.GoogleMap;
+import com.vaadin.tapio.googlemaps.client.LatLon;
 import com.vaadin.ui.Alignment;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Button.ClickEvent;
 import com.vaadin.ui.Button.ClickListener;
+import com.vaadin.ui.CheckBox;
 import com.vaadin.ui.Component;
+import com.vaadin.ui.GridLayout;
 import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Label;
+import com.vaadin.ui.TextArea;
 import com.vaadin.ui.TextField;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.Window;
 
 @Theme("customTheme")
 @SpringUI
+@VaadinServletConfiguration(widgetset = "com.vaadin.tapio.googlemaps.WidgetSet", productionMode = false, ui = MainUI.class)
 public class MainUI extends UI {
 	private static final String TEXTFIELD_WIDTH = "500px";
 
@@ -143,15 +159,278 @@ public class MainUI extends UI {
 	private VerticalLayout createStepUI(Step step) {
 		VerticalLayout main = new VerticalLayout();
 		main.setData(step);
+		main.setSpacing(true);
 
-		// main.addComponent(createTextItem("Step Name:", new
-		// ObjectProperty<String>(step.getName())));
-		// main.addComponent(createTextItem("Text:", new
-		// ObjectProperty<String>(step.getTask())));
-		// main.addComponent(createTextItem("Treasure:", new
-		// ObjectProperty<String>(step.getTreasure())));
-		// main.addComponent(createTextItem("Solution:", new
-		// ObjectProperty<String>(step.getSolution())));
+		GridLayout grid = new GridLayout(2, 1);
+		grid.setSpacing(true);
+		grid.setDefaultComponentAlignment(Alignment.MIDDLE_RIGHT);
+		main.addComponent(grid);
+
+		TextField name = new TextField();
+		name.setWidth(TEXTFIELD_WIDTH);
+		name.addTextChangeListener(new TextChangeListener() {
+			@Override
+			public void textChange(TextChangeEvent event) {
+				step.setName(event.getText());
+			}
+		});
+		grid.addComponents(new Label("Step Name:"), name);
+
+		TextField text = new TextField();
+		text.setWidth(TEXTFIELD_WIDTH);
+		text.addTextChangeListener(new TextChangeListener() {
+			@Override
+			public void textChange(TextChangeEvent event) {
+				step.setTask(event.getText());
+			}
+		});
+		grid.addComponents(new Label("Text:"), text);
+
+		TextField treasure = new TextField();
+		treasure.setWidth(TEXTFIELD_WIDTH);
+		treasure.addTextChangeListener(new TextChangeListener() {
+			@Override
+			public void textChange(TextChangeEvent event) {
+				step.setTreasure(event.getText());
+			}
+		});
+		grid.addComponents(new Label("Treasure:"), treasure);
+
+		TextField solution = new TextField();
+		solution.setWidth(TEXTFIELD_WIDTH);
+		solution.addTextChangeListener(new TextChangeListener() {
+			@Override
+			public void textChange(TextChangeEvent event) {
+				step.setSolution(event.getText());
+			}
+		});
+		grid.addComponents(new Label("Solution:"), solution);
+
+		ItemPanel points = new ItemPanel("Points:") {
+			@Override
+			public VerticalLayout addItem() {
+				Point point = new Point();
+				point.setHr(new HeartRate());
+				point.setName("");
+
+				step.getPoints().add(point);
+				return createPointUI(point);
+			}
+
+			@Override
+			public void removeItem(Object object) {
+				if (step.getPoints().contains(object)) {
+					step.getPoints().remove(object);
+				} else {
+					throw new RuntimeException("Object " + object
+							+ " does not exist.");
+				}
+			}
+		};
+		main.addComponent(points);
+
+		ItemPanel questions = new ItemPanel("Questions:") {
+			@Override
+			public VerticalLayout addItem() {
+				Question question = new Question();
+				question.setAnswers(new ArrayList<String>());
+				question.setCorrectAnswer("");
+				question.setQuestion("");
+
+				step.getQuestions().add(question);
+				
+				return createQuestionUI(question);
+			}
+
+			@Override
+			public void removeItem(Object object) {
+				if (step.getQuestions().contains(object)) {
+					step.getQuestions().remove(object);
+				} else {
+					throw new RuntimeException("Object " + object
+							+ " does not exist.");
+				}
+			}
+		};
+		main.addComponent(questions);
+
+		return main;
+	}
+
+	private VerticalLayout createPointUI(Point point) {
+		VerticalLayout root = new VerticalLayout();
+		root.setData(point);
+		
+		GridLayout main = new GridLayout(2, 1);
+		main.setSpacing(true);
+		main.setDefaultComponentAlignment(Alignment.MIDDLE_RIGHT);
+
+		TextField name = new TextField();
+		name.setWidth(TEXTFIELD_WIDTH);
+		name.addTextChangeListener(new TextChangeListener() {
+			@Override
+			public void textChange(TextChangeEvent event) {
+				point.setName(event.getText());
+			}
+		});
+		main.addComponents(new Label("Point Name:"), name);
+
+		// Map
+		TextField loc = new TextField();
+		loc.addTextChangeListener(new TextChangeListener() {
+			@Override
+			public void textChange(TextChangeEvent event) {
+				System.out.println("LatLng=" + event.getText());
+			}
+		});
+		Button mapButton = new Button();
+		mapButton.setIcon(FontAwesome.MAP_MARKER);
+		mapButton.addClickListener(new ClickListener() {
+			@Override
+			public void buttonClick(ClickEvent event) {
+				Window popup = new Window();
+				UI.getCurrent().addWindow(popup);
+				popup.setCaption("Select a location...");
+				popup.setDraggable(false);
+				popup.setModal(true);
+				popup.setHeight("200px");
+				popup.setWidth("200px");
+				popup.center();
+
+				GoogleMap googleMap = new GoogleMap(null, null, null);
+				googleMap.setCenter(new LatLon(55.9094, 3.3201));
+
+				VerticalLayout content = new VerticalLayout();
+				popup.setContent(content);
+				content.setSizeFull();
+
+				content.addComponent(googleMap);
+
+				popup.setVisible(true);
+			}
+		});
+		HorizontalLayout horiz = new HorizontalLayout(loc, mapButton);
+		main.addComponents(new Label("LatLng:"), horiz);
+		main.setComponentAlignment(horiz, Alignment.MIDDLE_LEFT);
+
+		TextField mrange = new TextField();
+		mrange.setWidth(TEXTFIELD_WIDTH);
+		mrange.addTextChangeListener(new TextChangeListener() {
+			@Override
+			public void textChange(TextChangeEvent event) {
+				point.setName(event.getText());
+			}
+		});
+		main.addComponents(new Label("M Range:"), mrange);
+
+		TextField srange = new TextField();
+		srange.setWidth(TEXTFIELD_WIDTH);
+		srange.addTextChangeListener(new TextChangeListener() {
+			@Override
+			public void textChange(TextChangeEvent event) {
+				point.setName(event.getText());
+			}
+		});
+		main.addComponents(new Label("S Range:"), srange);
+
+		root.addComponent(main);
+		return root;
+	}
+
+	private VerticalLayout createQuestionUI(Question question) {
+		VerticalLayout root = new VerticalLayout();
+		root.setData(question);
+		
+		GridLayout main = new GridLayout(2, 1);
+		main.setSpacing(true);
+		main.setDefaultComponentAlignment(Alignment.MIDDLE_RIGHT);
+
+		TextField questionText = new TextField();
+		questionText.setWidth(TEXTFIELD_WIDTH);
+		questionText.addTextChangeListener(new TextChangeListener() {
+			@Override
+			public void textChange(TextChangeEvent event) {
+				question.setQuestion(event.getText());
+			}
+		});
+		main.addComponents(new Label("Question:"), questionText);
+
+		ItemPanel answers = new ItemPanel("Answers:") {
+			List<CheckBox> checks = new ArrayList<CheckBox>();
+
+			@Override
+			public VerticalLayout addItem() {
+				return createAnswerUI(question, checks);
+			}
+
+			@Override
+			public void removeItem(Object object) {
+				CheckBox checkBox = (CheckBox) ((ArrayList) object).get(1);
+				if (question.getAnswers().contains(checkBox)) {
+					checks.remove(checkBox);
+				} else {
+					throw new RuntimeException("Object " + checkBox
+							+ " does not exist.");
+				}
+
+				Object value = ((ArrayList) object).get(0);
+				if (question.getAnswers().contains(value)) {
+					question.getAnswers().remove(value);
+				} else {
+					throw new RuntimeException("Object " + value
+							+ " does not exist.");
+				}
+			}
+		};
+		main.addComponent(answers);
+
+		root.addComponent(main);
+		return root;
+	}
+
+	private VerticalLayout createAnswerUI(Question question,
+			List<CheckBox> checks) {
+		VerticalLayout main = new VerticalLayout();
+		List<Object> data = new ArrayList<Object>();
+		data.add("");
+		main.setData(data);
+
+		TextArea answer = new TextArea();
+		answer.addTextChangeListener(new TextChangeListener() {
+			@Override
+			public void textChange(TextChangeEvent event) {
+				Object oldValue = data.get(0);
+				if (question.getAnswers().contains(oldValue)) {
+					question.getAnswers().remove(oldValue);
+					question.getAnswers().add(event.getText());
+					data.remove(0);
+					data.add(0, event.getText());
+				} else {
+					throw new RuntimeException("Object " + oldValue
+							+ " does not exist.");
+				}
+			}
+		});
+
+		CheckBox isCorrect = new CheckBox("isCorrect:");
+		isCorrect.addValueChangeListener(new ValueChangeListener(){
+			@Override
+			public void valueChange(ValueChangeEvent event) {
+				if((boolean)event.getProperty().getValue()){
+					for(CheckBox c : checks){
+						if(c!=isCorrect){
+							c.setValue(false);
+						}
+					}
+				}
+			}			
+		});
+		checks.add(isCorrect);
+		data.add(1, isCorrect);
+
+		main.addComponent(new HorizontalLayout(new Label("Question #:"),
+				isCorrect));
+		main.addComponent(answer);
 
 		return main;
 	}
